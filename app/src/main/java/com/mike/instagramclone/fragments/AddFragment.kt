8c65ac76ef942263312d850fd3787304efb4e564
CR7sewey <miguelmotacarvalho@gmail.com>
2025-04-02 23:08:20 +0100

    Fragment Add - Dialog + activities setup - 1

--- app/src/main/java/com/mike/instagramclone/fragments/AddFragment.kt
+++ app/src/main/java/com/mike/instagramclone/fragments/AddFragment.kt
@@ -1,11 +1,17 @@
 package com.mike.instagramclone.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.mike.instagramclone.Post.AddPostActivity
+import com.mike.instagramclone.Post.AddReelsActivity
 import com.mike.instagramclone.R
+import com.mike.instagramclone.databinding.FragmentAddBinding
+import com.mike.instagramclone.databinding.FragmentProfileBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +23,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [AddFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AddFragment : Fragment() {
+class AddFragment : BottomSheetDialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: FragmentAddBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +36,7 @@ class AddFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -35,7 +44,19 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        binding = FragmentAddBinding.inflate(inflater, container, false)
+
+        binding.addPost.setOnClickListener {
+            var intent = Intent(requireContext(), AddPostActivity::class.java)
+            activity?.startActivity(intent)
+        }
+        binding.addReel.setOnClickListener {
+            var intent = Intent(requireContext(), AddReelsActivity::class.java)
+            activity?.startActivity(intent)
+        }
+
+
+        return binding.root
     }
 
     companion object {
